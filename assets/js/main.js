3d185ac25d212420b6b235b0d1cd0c06ea11c1f8
Ganesh Babu H C <ganesh.babu.h.c@oracle.com>
2021-09-13 18:56:32 +0530

    Services changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,6 +68,25 @@ tabs.forEach((tab)=>{
 
 /*==================== SERVICES MODAL ====================*/
 
+const modalViews = document.querySelectorAll('.services__modal'),
+modalButtons = document.querySelectorAll('.services__button'),
+modalCloses = document.querySelectorAll('.services__modal-close')
+let modal = function(modalClick) {
+  modalViews[modalClick].classList.add('active-modal');
+}
+modalButtons.forEach((modalButton, i)=>{
+  modalButton.addEventListener('click',()=>{
+    modal(i);
+  });
+});
+modalCloses.forEach((modalClose)=>{
+  modalClose.addEventListener('click',()=>{
+    modalViews.forEach((modalView)=>{
+      modalView.classList.remove('active-modal');
+    });
+  });
+});
+
 /*==================== PORTFOLIO SWIPER  ====================*/
 
 /*==================== TESTIMONIAL ====================*/
