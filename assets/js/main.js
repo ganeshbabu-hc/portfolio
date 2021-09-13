828afd15071a77c6fd314a76924313336863411b
Ganesh Babu H C <ganesh.babu.h.c@oracle.com>
2021-09-13 15:58:13 +0530

    Qualification changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,6 +50,22 @@ skillHeader.forEach((el) => {
 
 /*==================== QUALIFICATION TABS ====================*/
 
+const tabs = document.querySelectorAll('[data-target]'),
+tabContent = document.querySelectorAll('[data-content]');
+tabs.forEach((tab)=>{
+  tab.addEventListener('click', ()=>{
+    const target = document.querySelector(tab.dataset.target);
+    tabContent.forEach((content)=>{
+      content.classList.remove('qualification__active');
+    });
+    target.classList.add('qualification__active');
+    tabs.forEach((tab)=>{
+      tab.classList.remove('qualification__active');
+    }); 
+    tab.classList.add('qualification__active');
+  });
+});
+
 /*==================== SERVICES MODAL ====================*/
 
 /*==================== PORTFOLIO SWIPER  ====================*/
