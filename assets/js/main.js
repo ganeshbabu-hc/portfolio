0da20d6390e0ff5b42ffafae255a2eb0cc4188bc
Ganesh Babu H C <ganesh.babu.h.c@oracle.com>
2021-09-13 20:05:27 +0530

    portfolio changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,44 +50,59 @@ skillHeader.forEach((el) => {
 
 /*==================== QUALIFICATION TABS ====================*/
 
-const tabs = document.querySelectorAll('[data-target]'),
-tabContent = document.querySelectorAll('[data-content]');
-tabs.forEach((tab)=>{
-  tab.addEventListener('click', ()=>{
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContent = document.querySelectorAll("[data-content]");
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
     const target = document.querySelector(tab.dataset.target);
-    tabContent.forEach((content)=>{
-      content.classList.remove('qualification__active');
+    tabContent.forEach((content) => {
+      content.classList.remove("qualification__active");
     });
-    target.classList.add('qualification__active');
-    tabs.forEach((tab)=>{
-      tab.classList.remove('qualification__active');
-    }); 
-    tab.classList.add('qualification__active');
+    target.classList.add("qualification__active");
+    tabs.forEach((tab) => {
+      tab.classList.remove("qualification__active");
+    });
+    tab.classList.add("qualification__active");
   });
 });
 
 /*==================== SERVICES MODAL ====================*/
 
-const modalViews = document.querySelectorAll('.services__modal'),
-modalButtons = document.querySelectorAll('.services__button'),
-modalCloses = document.querySelectorAll('.services__modal-close')
-let modal = function(modalClick) {
-  modalViews[modalClick].classList.add('active-modal');
-}
-modalButtons.forEach((modalButton, i)=>{
-  modalButton.addEventListener('click',()=>{
+const modalViews = document.querySelectorAll(".services__modal"),
+  modalButtons = document.querySelectorAll(".services__button"),
+  modalCloses = document.querySelectorAll(".services__modal-close");
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+modalButtons.forEach((modalButton, i) => {
+  modalButton.addEventListener("click", () => {
     modal(i);
   });
 });
-modalCloses.forEach((modalClose)=>{
-  modalClose.addEventListener('click',()=>{
-    modalViews.forEach((modalView)=>{
-      modalView.classList.remove('active-modal');
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
     });
   });
 });
 
 /*==================== PORTFOLIO SWIPER  ====================*/
+
+const swiper = new Swiper(".swiper-container", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:true
+  },
+  mousewheel: true,
+  keyboard: true,
+});
 
 /*==================== TESTIMONIAL ====================*/
 
